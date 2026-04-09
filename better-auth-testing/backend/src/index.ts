c36a873aa946e996efd1077a7c72a0b1bbb0228b
Chillie <reskialamsyah@gmail.com>
2026-04-09 19:49:57 +0800

    add cors and auth route

--- better-auth-testing/backend/src/index.ts
+++ better-auth-testing/backend/src/index.ts
@@ -1,9 +1,25 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import authRoute from "@routes/auth.route";
 
 const app = new Hono();
 
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:5173",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
+  }),
+);
+
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+  return c.text("Hello Hono!");
 });
+
+app.route("/api/auth", authRoute);
 
 export default app;
