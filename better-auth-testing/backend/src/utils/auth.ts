a0032e411ee8f5e8a8ecdb8fac4839bd84f534bd
Chillie <reskialamsyah@gmail.com>
2026-04-09 19:50:04 +0800

    better auth config

---
+++ better-auth-testing/backend/src/utils/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import env from "@config/env";
+import db from "@config/database";
+
+export const auth = betterAuth({
+  database: db,
+
+  baseURL: env.BETTER_AUTH_URL || "http://localhost:3000",
+  secret: env.BETTER_AUTH_SECRET,
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID as string,
+      clientSecret: env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
+  trustedOrigins: ["http://localhost/5173"],
+});
