6fcd177986e6b181188d3c612eab1a461934e99c
Chillie <reskialamsyah@gmail.com>
2026-04-09 23:46:23 +0800

    show session email and name if login/signup success and add button to signout

--- better-auth-testing/frontend/src/App.tsx
+++ better-auth-testing/frontend/src/App.tsx
@@ -1,11 +1,53 @@
-import Signup from "~/components/Signup";
+import Signup from "@components/Signup";
+import Loader from "@components/Loader";
+import Login from "@components/Login";
 import { Activity, useState } from "react";
-import Login from "./components/Login";
+import { Toaster } from "react-hot-toast";
+import authClient from "@utils/auth-client";
 
 function App() {
   const [view, setView] = useState<"login" | "signup">("login");
+  const { data: session, isPending } = authClient.useSession();
+
+  if (isPending) {
+    return (
+      <div className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center gap-6 px-4">
+        <main className="grid w-full flex-1 place-items-center">
+          <Loader></Loader>
+        </main>
+      </div>
+    );
+  }
+
+  if (session) {
+    const user = {
+      email: session.user.email,
+      name: session.user.name,
+    };
+
+    return (
+      <div className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center gap-6 px-4">
+        <main className="flex w-full flex-1 flex-col justify-center gap-3">
+          <header>
+            <h1 className="text-2xl font-medium">Hello, {user.name}</h1>
+            <p className="text-sm text-gray-500">{user.email}</p>
+          </header>
+
+          <button
+            className="bg-gray-300 px-4 py-2 transition-colors hover:bg-gray-400 w-fit rounded-md font-semibold"
+            type="button"
+            onClick={() => authClient.signOut()}
+          >
+            Sign Out
+          </button>
+        </main>
+      </div>
+    );
+  }
+
   return (
     <>
+      <Toaster></Toaster>
       <div className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center gap-6 px-4">
         <nav className="space-x-2 rounded-sm bg-gray-200 p-1">
           <button
