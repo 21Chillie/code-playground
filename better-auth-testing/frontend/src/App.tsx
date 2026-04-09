ef3800a30aef9802fcda2a358cf169a32be049b2
Chillie <reskialamsyah@gmail.com>
2026-04-09 21:14:12 +0800

    add view state to see login or signup form

--- better-auth-testing/frontend/src/App.tsx
+++ better-auth-testing/frontend/src/App.tsx
@@ -1,121 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Signup from "~/components/Signup";
+import { Activity, useState } from "react";
+import Login from "./components/Login";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [view, setView] = useState<"login" | "signup">("login");
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <div className="mx-auto flex min-h-screen max-w-xl flex-col items-center justify-center gap-6 px-4">
+        <nav className="space-x-2 rounded-sm bg-gray-200 p-1">
+          <button
+            type="button"
+            className={`${view === "login" && "bg-gray-300"} rounded-sm px-4 py-2 text-lg font-semibold text-gray-700 transition-colors hover:bg-gray-300`}
+            onClick={() => setView("login")}
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            className={`${view === "signup" && "bg-gray-300"} rounded-sm px-4 py-2 text-lg font-semibold text-gray-700 transition-colors hover:bg-gray-300`}
+            onClick={() => setView("signup")}
+          >
+            Sign Up
+          </button>
+        </nav>
 
-      <div className="ticks"></div>
+        <main className="w-full">
+          <Activity mode={view === "login" ? "visible" : "hidden"}>
+            <Login></Login>
+          </Activity>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          <Activity mode={view === "signup" ? "visible" : "hidden"}>
+            <Signup></Signup>
+          </Activity>
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
