51d0db814f7d24169554da62b0420be47ae68f04
Chillie <reskialamsyah@gmail.com>
2026-06-04 20:26:42 +0800

    update: implement persist middleware from zustand to store state value in local storage

--- zustand-state-management/src/store/useBoundStore.ts
+++ zustand-state-management/src/store/useBoundStore.ts
@@ -1,8 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { countSlice, type CounterState } from "./countSlice";
 import { themeSlice, type ThemeState } from "./themeSlice";
 
-export const useBoundStore = create<CounterState & ThemeState>()((...a) => ({
-  ...countSlice(...a),
-  ...themeSlice(...a),
-}));
+// Persist the useBoundStore state
+export const useBoundStore = create<CounterState & ThemeState>()(
+  persist(
+    (...a) => ({
+      ...countSlice(...a),
+      ...themeSlice(...a),
+    }),
+
+    // Store it with key `bound-store`
+    // And only persist the `theme` state value and store it in localStorage
+    // NOTE: that you can partialize more than 1 state, just add more property
+    { name: "bound-store", partialize: (state) => ({ theme: state.theme }) }
+  )
+);
